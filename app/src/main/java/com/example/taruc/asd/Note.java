67934ed7d38e3d13aa4eabfa84c05fe4f3688144
anreh <anurehka@gmail.com>
2018-10-18 12:51:06 +0800

    added a new commend at Note.java

--- app/src/main/java/com/example/taruc/asd/Note.java
+++ app/src/main/java/com/example/taruc/asd/Note.java
@@ -18,6 +18,8 @@ public class Note extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //babababa
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
